b4e7a2d93fbd26d3c059ee6a4bcfd6e808e8266a
xgfone <xgfone@126.com>
2021-03-20 16:43:39 +0800

    add the test for the default host

--- gateway_test.go
+++ gateway_test.go
@@ -65,16 +65,14 @@ func TestGateway_NotFound(t *testing.T) {
 	rec = httptest.NewRecorder()
 	req, _ = http.NewRequest(http.MethodGet, "http://www.example.com", nil)
 	gw.ServeHTTP(rec, req)
-	if s := rec.Body.String(); s != "OK" {
-		t.Errorf("expect '%s', but got '%s'\n", "OK", s)
+	if s := rec.Body.String(); s != "test" {
+		t.Errorf("expect '%s', but got '%s'\n", "test", s)
 	}
 }
 
-type testForwarder struct {
-	name string
-}
+type testForwarder struct{ name string }
 
-func (f testForwarder) Forward(c *apigw.Context) error { return c.Text(200, "OK") }
+func (f testForwarder) Forward(c *apigw.Context) error { return c.Text(200, f.name) }
 func (f testForwarder) Name() string                   { return f.name }
 func (f testForwarder) Close() error                   { return nil }
 
@@ -105,8 +103,8 @@ func TestGatewayMiddleware(t *testing.T) {
 	rec := httptest.NewRecorder()
 	req, _ := http.NewRequest(http.MethodGet, "http://www.example.com", nil)
 	gw.ServeHTTP(rec, req)
-	if rec.Code != 200 || rec.Body.String() != "OK" {
-		t.Errorf("code: %d != 200, body: '%s' != 'OK'", rec.Code, rec.Body.String())
+	if rec.Code != 200 || rec.Body.String() != "test" {
+		t.Errorf("code: %d != 200, body: '%s' != 'test'", rec.Code, rec.Body.String())
 	}
 
 	lines := strings.Split(strings.TrimSpace(buf.String()), "\n")
@@ -147,5 +145,73 @@ func testMiddleware(host string, index int, buf *bytes.Buffer) apigw.Middleware 
 			fmt.Fprintf(buf, "%s %d after\n", host, index)
 			return
 		}
+	}
+}
+
+func panicError(t *testing.T, name string, err error) {
+	if err != nil {
+		t.Fatalf("%s: %s", name, err)
+	}
+}
+
+func TestGateway_SetDefaultHost(t *testing.T) {
+	g := apigw.NewGateway()
+	panicError(t, "www.example.com", g.AddHost("www.example.com"))
+	panicError(t, "www1.example.com", g.AddHost("www1.example.com"))
+	panicError(t, "set_default_host", g.SetDefaultHost("www.example.com"))
+
+	if host, _ := g.Router().GetDefaultRouter(); host != "www.example.com" {
+		t.Errorf("expect host '%s', but got '%s'", "www.example.com", host)
+	}
+
+	_, err := g.RegisterRoute(apigw.Route{
+		Host:      "www.example.com",
+		Path:      "/",
+		Method:    http.MethodGet,
+		Forwarder: testForwarder{"route"},
+	})
+	panicError(t, "register_route1", err)
+
+	_, err = g.RegisterRoute(apigw.Route{
+		Host:      "www1.example.com",
+		Path:      "/",
+		Method:    http.MethodGet,
+		Forwarder: testForwarder{"route1"},
+	})
+	panicError(t, "register_route2", err)
+
+	if routers := g.Router().Routers(); len(routers) != 2 {
+		t.Errorf("expect %d hosts ,but got %d", 2, len(routers))
+	} else {
+		for host, router := range routers {
+			switch host {
+			case "www.example.com":
+				if rs := router.Routes(); len(rs) != 1 {
+					t.Errorf("wrong routes: %v", rs)
+				}
+			case "www1.example.com":
+				if rs := router.Routes(); len(rs) != 1 {
+					t.Errorf("wrong routes: %v", rs)
+				}
+			default:
+				t.Errorf("unknown host '%s'", host)
+			}
+		}
+	}
+
+	rec := httptest.NewRecorder()
+	req, err := http.NewRequest(http.MethodGet, "http://www1.example.com", nil)
+	panicError(t, "new_request1", err)
+	if g.ServeHTTP(rec, req); rec.Code != 200 || rec.Body.String() != "route1" {
+		t.Errorf("expect: code=%d, body=%s, but got: code=%d, body=%s",
+			200, "route1", rec.Code, rec.Body.String())
+	}
+
+	rec = httptest.NewRecorder()
+	req, err = http.NewRequest(http.MethodGet, "http://www2.example.com", nil)
+	panicError(t, "new_request2", err)
+	if g.ServeHTTP(rec, req); rec.Code != 200 || rec.Body.String() != "route" {
+		t.Errorf("expect: code=%d, body=%s, but got: code=%d, body=%s",
+			200, "route", rec.Code, rec.Body.String())
 	}
 }
